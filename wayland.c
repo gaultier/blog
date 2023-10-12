937e2c3a8f5861518d42076389924f2de13e4fac
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-10-12 13:16:10 +0200

    stop hard-coding socket path

--- wayland.c
+++ wayland.c
@@ -82,22 +82,27 @@ static int wayland_display_connect() {
 
   uint64_t xdg_runtime_dir_len = strlen(xdg_runtime_dir);
 
-  char socket_path[4096] = "";
+  struct sockaddr_un addr = {.sun_family = AF_UNIX};
+  assert(xdg_runtime_dir_len <= cstring_len(addr.sun_path));
   uint64_t socket_path_len = 0;
-  memcpy(socket_path, xdg_runtime_dir, xdg_runtime_dir_len);
+
+  memcpy(addr.sun_path, xdg_runtime_dir, xdg_runtime_dir_len);
   socket_path_len += xdg_runtime_dir_len;
+
+  addr.sun_path[socket_path_len++] = '/';
 
   char *wayland_display = getenv("WAYLAND_DISPLAY");
   if (wayland_display == NULL) {
     char wayland_display_default[] = "wayland-0";
     uint64_t wayland_display_default_len = cstring_len(wayland_display_default);
 
-    memcpy(socket_path + socket_path_len, wayland_display_default,
+    memcpy(addr.sun_path + socket_path_len, wayland_display_default,
            wayland_display_default_len);
     socket_path_len += wayland_display_default_len;
   } else {
     uint64_t wayland_display_len = strlen(wayland_display);
-    memcpy(socket_path + socket_path_len, wayland_display, wayland_display_len);
+    memcpy(addr.sun_path + socket_path_len, wayland_display,
+           wayland_display_len);
     socket_path_len += wayland_display_len;
   }
 
@@ -105,8 +110,6 @@ static int wayland_display_connect() {
   if (fd == -1)
     exit(errno);
 
-  struct sockaddr_un addr = {.sun_family = AF_UNIX,
-                             .sun_path = "/run/user/1000/wayland-1"};
   if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
     exit(errno);
 
