c14d4f7e31a5de91e889830f699bda4299339998
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-06-21 15:27:37 +0200

    [wip]

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,39 @@ const std = @import("std");
 
 pub const std_options = .{
     .log_level = .info,
+    .logFn = myLogFn,
 };
+
+pub fn myLogFn(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const prefix = "[" ++ comptime level.asText() ++ "] " ++ @tagName(scope);
+
+    // Print the message to stderr, silently ignoring any errors
+    std.debug.lockStdErr();
+    defer std.debug.unlockStdErr();
+    const stderr = std.io.getStdErr().writer();
+    nosuspend stderr.print(prefix ++ format ++ "\n", args) catch return;
+}
 
 const html_prelude = "<!DOCTYPE html>\n<html>\n<head>\n<title>{s}</title>\n";
 
+fn do_generate_article(markdown_file_path: []const u8, header: []const u8, footer: []const u8, wait_group: *std.Thread.WaitGroup, allocator: std.mem.Allocator) void {
+    defer wait_group.finish();
+
+    generate_article(markdown_file_path, header, footer, allocator) catch |err| {
+        std.log.err("failed to generate article: {s} {}", .{ markdown_file_path, err });
+    };
+}
+
 fn generate_article(markdown_file_path: []const u8, header: []const u8, footer: []const u8, allocator: std.mem.Allocator) !void {
+    std.debug.assert(std.mem.eql(u8, std.fs.path.extension(markdown_file_path), ".md"));
+    std.debug.assert(header.len > 0);
+    std.debug.assert(footer.len > 0);
+
     const is_index_page = std.mem.eql(u8, markdown_file_path, "index.md");
 
     const markdown_file = try std.fs.cwd().openFile(markdown_file_path, .{});
@@ -42,6 +70,7 @@ fn generate_article(markdown_file_path: []const u8, header: []const u8, footer: 
         std.debug.assert(git_output.stderr.len == 0);
 
         const first_newline_pos = std.mem.indexOf(u8, git_output.stdout, "\n") orelse 0;
+        std.log.info("[D001] {s} {} {s}", .{ markdown_file_path, first_newline_pos, git_output.stdout });
         std.debug.assert(first_newline_pos > 0);
 
         const markdown_file_creation_date = git_output.stdout[0..first_newline_pos];
@@ -65,10 +94,8 @@ fn generate_article(markdown_file_path: []const u8, header: []const u8, footer: 
 }
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-
-    const allocator = arena.allocator();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
     const header_file = try std.fs.cwd().openFile("header.html", .{});
     const header = try header_file.readToEndAlloc(allocator, 2048);
@@ -79,17 +106,30 @@ pub fn main() !void {
     const cwd = try std.fs.cwd().openDir(".", .{ .iterate = true });
     var cwd_iterator = cwd.iterate();
 
+    var pool: std.Thread.Pool = undefined;
+    try std.Thread.Pool.init(&pool, .{ .allocator = allocator });
+    var wait_group = std.Thread.WaitGroup{};
+
     while (cwd_iterator.next()) |entry_opt| {
         if (entry_opt) |entry| {
             // Skip non markdown files.
             if (entry.kind != .file) continue;
             if (!std.mem.eql(u8, std.fs.path.extension(entry.name), ".md")) continue;
 
-            if (std.mem.eql(u8, entry.name, "README.md")) continue; // Skip.
+            if (std.mem.eql(u8, entry.name, "README.md")) continue;
 
-            try generate_article(entry.name, header, footer, allocator);
+            wait_group.start();
+            pool.spawnWg(&wait_group, do_generate_article, .{
+                entry.name,
+                header,
+                footer,
+                &wait_group,
+                allocator,
+            });
         } else break; // End of directory.
     } else |err| {
         std.log.err("failed to iterate over directory entries: {}", .{err});
     }
+
+    wait_group.wait();
 }
