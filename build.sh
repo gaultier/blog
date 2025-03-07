3f98018ca291fbfd64240539ed330c8c22372a28
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-03-07 09:18:35 +0100

    [wip]

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e
+set -f # disable globbing.
+
+CFLAGS="${CFLAGS} -fpie -fno-omit-frame-pointer -gsplit-dwarf -march=native -fuse-ld=lld"
+LDFLAGS="${LDFLAGS} -Wl,--gc-sections -flto"
+
+CC="${CC:-clang}"
+WARNINGS="$(tr -s '\n' ' ' < compile_flags.txt)"
+
+error() {
+	printf "ERROR: %s\n" "$1"
+	exit 1
+}
+
+build() {
+case $1 in 
+  debug)
+    CFLAGS="${CFLAGS} -O0"
+    ;;
+  debug_sanitizer)
+    CFLAGS="${CFLAGS} -O0 -fsanitize=address,undefined -fsanitize-trap=all"
+    ;;
+  release)
+    CFLAGS="${CFLAGS} -O3"
+    ;;
+  release_sanitizer)
+    CFLAGS="${CFLAGS} -O1 -fsanitize=address,undefined -fsanitize-trap=all"
+    ;;
+	*)
+		error "Build mode \"$1\" unsupported!"
+		;;
+esac
+
+# shellcheck disable=SC2086
+$CC $WARNINGS -g3 main.c -o main.bin $CFLAGS $LDFLAGS
+}
+
+if [ $# -eq 0 ]; then
+	build debug
+elif [ $# -eq 1 ]; then
+  build "$1"
+else
+	error "Too many arguments!"
+fi
---
+++ main.c
@@ -0,0 +1,49 @@
+#include "./submodules/cstd/lib.c"
+
+typedef struct {
+  u32 value;
+} Sha256Hash;
+
+typedef struct Title Title;
+struct Title {
+  PgString title;
+  PgString content_html_id;
+  u8 level;
+  Sha256Hash hash;
+  Title *parent, *first_child, *next_sibling;
+  u32 pos_start, pos_end;
+};
+
+typedef struct {
+  PgString html_file_name;
+  PgString title;
+  PgStringSlice tags;
+  PgString creation_date, modification_date;
+  Title *titles_root;
+} Article;
+PG_DYN(Article) ArticleDyn;
+PG_SLICE(Article) ArticleSlice;
+
+static ArticleSlice articles_generate(PgString header, PgString footer,
+                                      PgAllocator *allocator) {
+  ArticleDyn articles = {0};
+  PG_DYN_ENSURE_CAP(&articles, 100, allocator);
+
+  return PG_DYN_SLICE(ArticleSlice, articles);
+}
+
+int main() {
+  PgArena arena = pg_arena_make_from_virtual_mem(10 * PG_MiB);
+  PgArenaAllocator arena_allocator = pg_make_arena_allocator(&arena);
+  PgAllocator *allocator = pg_arena_allocator_as_allocator(&arena_allocator);
+
+  PgStringResult res_header =
+      pg_file_read_full_from_path(PG_S("header.html"), allocator);
+  PG_ASSERT(0 == res_header.err);
+  PgString header = res_header.res;
+
+  PgStringResult res_footer =
+      pg_file_read_full_from_path(PG_S("footer.html"), allocator);
+  PG_ASSERT(0 == res_footer.err);
+  PgString footer = res_footer.res;
+}
