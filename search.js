20b56a7e75a3e156e4ca810c22080af096900dde
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-03-18 09:29:31 +0100

    [wip]

--- search.js
+++ search.js
@@ -3,9 +3,10 @@ const {raw_index}=search;
 
 function search_text(needle) {
   console.time("search_text");
+  console.log("[D000]", needle, raw_index.documents.length);
 
   const matches=[];
-  for (let [doc_i, doc] in raw_index.documents.entries()){
+  for (let [doc_i, doc] of raw_index.documents.entries()){
     const idx = doc.text.indexOf(needle);
     if (-1==idx) { continue; }
     matches.push([doc_i | 0,idx]);
@@ -31,7 +32,7 @@ function search_text(needle) {
     let excerpt_idx_end = idx + needle.length + excerpt_len_around;
 
     const excerpt = doc.text.slice(excerpt_idx_start, excerpt_idx_end).trim().replace('\n',' ');
-    console.log("[D004]", pos, title_i, doc.name, title, link, "`"+ excerpt + "`", idx - excerpt_len_around);
+    console.log("[D004]", title_i, doc.name, title, link, "`"+ excerpt + "`", idx - excerpt_len_around);
 
     res.push({
       title: title,
@@ -43,3 +44,33 @@ function search_text(needle) {
 
   return res;
 }
+
+window.onload = function(){
+  const search_results = document.getElementById('search-results');
+  const input_elem = document.getElementById('search');
+  input_elem.oninput = function(event){
+    search_results.innerHTML = '';
+
+    console.log(event);
+    const matches = search_text(input_elem.value);
+
+    for (const match of matches.values()) {
+      const elem = document.createElement('p');
+
+      const document_name = document.createElement('span');
+      document_name.innerText = match.document_name;
+      elem.append(document_name);
+
+      const excerpt = document.createElement('span');
+      excerpt.innerText = match.excerpt;
+      elem.append(excerpt);
+
+      const link = document.createElement('a');
+      link.setAttribute('href', '/blog/' + match.link);
+      link.innerText = match.title;
+      elem.append(link);
+
+      search_results.prepend(elem);
+    }
+  };
+};
