0fef3ec60eaa6442c9f07c7f9f16f4ad51893aa4
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-03-18 08:56:48 +0100

    [wip]

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ import search from './search_index.js';
 const {raw_index}=search;
 
 console.time("search_text_without_index");
-const needle = 'Rust';
+const needle = 'I am';
 const res=[];
 for (let doc_i in raw_index.documents){
   const doc = raw_index.documents[doc_i];
@@ -27,11 +27,9 @@ for (let pos of res) {
   const title = doc.titles[title_i];
   const link = doc.name + title
   const excerpt_len_around = 30;
-  let excerpt_idx_start = idx - excerpt_len_around;
+  let excerpt_idx_start = idx - excerpt_len_around < 0 ? 0 : idx - excerpt_len_around;
   let excerpt_idx_end = idx + needle.length + excerpt_len_around;
-  // while (excerpt_idx_start >= idx - excerpt_len_around && doc.text[excerpt_idx_start] != '\n') excerpt_idx_start--; 
-  // while (excerpt_idx_end <= idx + excerpt_len_around && doc.text[excerpt_idx_end] != '\n') excerpt_idx_end++; 
 
-  const excerpt = doc.text.slice(excerpt_idx_start, excerpt_idx_end).trim();
-  console.log("[D004]", pos, title_i, doc.name, title, link, "`"+ excerpt + "`");
+  const excerpt = doc.text.slice(excerpt_idx_start, excerpt_idx_end).trim().replace('\n',' ');
+  console.log("[D004]", pos, title_i, doc.name, title, link, "`"+ excerpt + "`", idx - excerpt_len_around);
 }
