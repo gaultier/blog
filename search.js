ed565a2147a4c8f4488ef5dc0a761564eb53676f
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-03-18 08:37:22 +0100

    [wip]

--- search.js
+++ search.js
@@ -1,64 +1,32 @@
 import search from './search_index.js';
 const {raw_index}=search;
 
-function search_unpack_trigram_position(position){
-  return {
-    document_index : (position >> 0) & 0xfff,
-    // offset_start : (position >> 12) & 0xfff,
-    // offset_end : (position >> 24) & 0xfff,
-    section : (position >> 12) & 0xff,
-  }; 
-}
-
-function search_trigram(raw_index, trigram) {
-  const v = raw_index.index[trigram];
-  if (v == undefined) {
-    return undefined;
-  }
-
-  return new Set(v);
-}
-
-function search_text(raw_index, text) {
-  let results = undefined;
-
-  const runes = Array.from(text);
-  if (runes.length < 3){ return undefined; }
-
-  for (let i=2; i<runes.length; i++){
-    const trigram = runes[i-2]+runes[i-1]+runes[i];
-    const trigram_search = search_trigram(raw_index, trigram);
-    console.log("[D001]", i, trigram, trigram_search, results);
-    results = results ? results.intersection(trigram_search) : trigram_search;
-    console.log("[D002]", i, trigram, trigram_search, results);
-  }
-
-
-  return results;
-}
-
-console.time("search_text_with_index");
-const res = search_text(raw_index, 'Odin');
-console.timeEnd("search_text_with_index");
-
 console.time("search_text_without_index");
 const needle = 'Odin';
-const res2=[];
-for (let doc of raw_index.documents){
+const res=[];
+for (let doc_i in raw_index.documents){
+  const doc = raw_index.documents[doc_i];
   const idx = doc.text.indexOf(needle);
   if (-1==idx) { continue; }
-  res2.push([doc,idx]);
+  res.push([doc_i | 0,idx]);
 }
 console.timeEnd("search_text_without_index");
 
-console.log("[D010]", res2);
+console.log("[D010]", res);
 
-console.log("[D003]", res);
 for (let pos of res) {
-  const unpacked = search_unpack_trigram_position(pos);
-  const doc = raw_index.documents[unpacked.document_index];
-  const title = doc.titles[unpacked.section];
-  const link = doc.name + title
-  console.log("[D004]",pos, unpacked, doc.name, title, link);
-}
+  const [doc_i, idx] = pos;
+  const doc = raw_index.documents[doc_i];
 
+  let title_i = 0;
+  for (title_i of doc.title_text_offsets.keys()){
+    const offset = doc.title_text_offsets[title_i];
+    if (offset >= idx) {break;}
+  }
+
+  const title = doc.titles[title_i];
+  const link = doc.name + title
+  const excerpt_len_around = 10;
+  const excerpt = doc.text.slice(idx - excerpt_len_around, idx + needle.length + excerpt_len_around);
+  console.log("[D004]", pos, title_i, doc.name, title, link, excerpt, idx - excerpt_len_around, idx + needle.length + excerpt_len_around);
+}
