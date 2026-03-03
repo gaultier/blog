47eba50a6b8948ba6ba47ca7439cfba58198c5bf
Philippe Gaultier <philippe.gaultier@ory.sh>
2026-03-03 12:33:17 +0100

    run web server

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ use std::{
     cmp::Ordering,
     collections::{BTreeMap, HashMap},
     ffi::OsString,
-    fmt::Debug,
     fs::{self},
     path::{Path, PathBuf},
     process::Command,
+    thread,
     time::Duration,
 };
 
@@ -861,10 +861,10 @@ fn generate_home_page(articles: &mut [Article], html_header: &[u8], html_footer:
 }
 
 fn main() -> Result<()> {
-    let git_stats = git_get_articles_stats();
     let html_header = fs::read("header.html").unwrap();
     let html_footer = fs::read("footer.html").unwrap();
 
+    let git_stats = git_get_articles_stats();
     let mut articles: Vec<Article> = git_stats
         .into_iter()
         .filter(|gs| {
@@ -884,55 +884,30 @@ fn main() -> Result<()> {
 
     println!("generated {} articles", articles.len());
 
-    Ok(())
+    rouille::start_server("localhost:8001", move |request| {
+        dbg!(&request);
 
-    //rouille::start_server("localhost:8001", move |request| {
-    //    {
-    //        // The `match_assets` function tries to find a file whose name corresponds to the URL
-    //        // of the request. The second parameter (`"."`) tells where the files to look for are
-    //        // located.
-    //        // In order to avoid potential security threats, `match_assets` will never return any
-    //        // file outside of this directory even if the URL is for example `/../../foo.txt`.
-    //        let response = rouille::match_assets(request, "..");
-    //
-    //        // If a file is found, the `match_assets` function will return a response with a 200
-    //        // status code and the content of the file. If no file is found, it will instead return
-    //        // an empty 404 response.
-    //        // Here we check whether if a file is found, and if so we return the response.
-    //        if response.is_success() {
-    //            return response;
-    //        }
-    //    }
-    //
-    //    router!(request,
-    //
-    //
-    //
-    //        (GET) (/ws) => {
-    //            // This is the websockets route.
-    //
-    //            // In order to start using websockets we call `websocket::start`.
-    //            // The function returns an error if the client didn't request websockets, in which
-    //            // case we return an error 400 to the client thanks to the `try_or_400!` macro.
-    //            //
-    //            // The function returns a response to send back as part of the `start_server`
-    //            // function, and a `websocket` variable of type `Receiver<Websocket>`.
-    //            // Once the response has been sent back to the client, the `Receiver` will be
-    //            // filled by rouille with a `Websocket` object representing the websocket.
-    //
-    //            let (response, websocket) = try_or_400!(websocket::start(request, Some("echo")));
-    //
-    //            let watching_path = request.url();
-    //            thread::spawn(move || {
-    //                // This line will block until the `response` above has been returned.
-    //                let ws = websocket.recv().unwrap();
-    //                websocket_handling_thread(ws, watching_path);
-    //            });
-    //            response
-    //        },
-    //        _ => rouille::Response::empty_404()
-    //    )
-    //});
+        router!(request,
+            (GET) (/blog) => {
+                 rouille::Response::redirect_302("/blog/index.html")
+            },
+            (GET) (/blog/) => {
+                 rouille::Response::redirect_302("/blog/index.html")
+            },
+            (GET) (/ws) => {
+                let (response, websocket) = try_or_400!(websocket::start(request, Some("echo")));
+
+                let watching_path = request.url();
+                thread::spawn(move || {
+                    // This line will block until the `response` above has been returned.
+                    let ws = websocket.recv().unwrap();
+                    websocket_handling_thread(ws, watching_path);
+                });
+                response
+            },
+            _ => rouille::match_assets(request, "..")
+        )
+    });
 }
 
 fn websocket_handling_thread(mut websocket: websocket::Websocket, _watching_path: String) {
