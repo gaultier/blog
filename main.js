2c8b8a6c4d34c79d3792b9d53f816c5982a39e8e
Philippe Gaultier <philippe.gaultier@ory.sh>
2026-03-04 15:15:19 +0100

    [wip]

--- main.js
+++ main.js
@@ -14,61 +14,61 @@ socket.onerror = function(ev) {
   console.log('error', ev);
 }
 
-hljs.registerLanguage("odin", function(e) {
-return {
-    aliases: ["odin", "odinlang", "odin-lang"],
-    keywords: {
-        keyword: "auto_cast bit_field bit_set break case cast context continue defer distinct do dynamic else enum fallthrough for foreign if import in map matrix not_in or_else or_return package proc return struct switch transmute type_of typeid union using when where",
-        literal: "true false nil",
-        built_in: "abs align_of cap clamp complex conj expand_to_tuple imag jmag kmag len max min offset_of quaternion real size_of soa_unzip soa_zip swizzle type_info_of type_of typeid_of"
-    },
-    illegal: "</",
-    contains: [e.C_LINE_COMMENT_MODE, e.C_BLOCK_COMMENT_MODE, {
-        className: "string",
-        variants: [e.QUOTE_STRING_MODE, {
-            begin: "'",
-            end: "[^\\\\]'"
-        }, {
-            begin: "`",
-            end: "`"
-        }]
-    }, {
-        className: "number",
-        variants: [{
-            begin: e.C_NUMBER_RE + "[ijk]",
-            relevance: 1
-        }, e.C_NUMBER_MODE]
-    }]
-}
-});
-
-hljs.registerLanguage("awk", function(e) {
-return {
-    aliases: ["awk", "awklang", "awk-lang"],
-    keywords: {
-        keyword: "if else print break printf while for in return skip BEGIN END",
-        literal: "",
-        built_in: ""
-    },
-    illegal: "</",
-  contains: [ hljs.COMMENT('#', '$'), ]
-}
-});
-
-hljs.registerLanguage("dtrace", function(e) {
-return {
-    aliases: ["d", "dtracelang", "dtrace-lang"],
-    keywords: {
-        keyword: "if else self this BEGIN END typedef struct uintptr_t uint8_t uint16_t uint32_t uint64_t intptr_t int8_t int16_t int32_t int64_t size_t void",
-        literal: "",
-        built_in: "trace copyin copyinstr stringof uregs print printf exit rindex strlen quantize lquantize timestamp execname pid $target io start args"
-    },
-    illegal: "</",
-  contains: [ hljs.COMMENT('//', '$'), ]
-}
-});
-
-hljs.highlightAll();
+// hljs.registerLanguage("odin", function(e) {
+// return {
+//     aliases: ["odin", "odinlang", "odin-lang"],
+//     keywords: {
+//         keyword: "auto_cast bit_field bit_set break case cast context continue defer distinct do dynamic else enum fallthrough for foreign if import in map matrix not_in or_else or_return package proc return struct switch transmute type_of typeid union using when where",
+//         literal: "true false nil",
+//         built_in: "abs align_of cap clamp complex conj expand_to_tuple imag jmag kmag len max min offset_of quaternion real size_of soa_unzip soa_zip swizzle type_info_of type_of typeid_of"
+//     },
+//     illegal: "</",
+//     contains: [e.C_LINE_COMMENT_MODE, e.C_BLOCK_COMMENT_MODE, {
+//         className: "string",
+//         variants: [e.QUOTE_STRING_MODE, {
+//             begin: "'",
+//             end: "[^\\\\]'"
+//         }, {
+//             begin: "`",
+//             end: "`"
+//         }]
+//     }, {
+//         className: "number",
+//         variants: [{
+//             begin: e.C_NUMBER_RE + "[ijk]",
+//             relevance: 1
+//         }, e.C_NUMBER_MODE]
+//     }]
+// }
+// });
+//
+// hljs.registerLanguage("awk", function(e) {
+// return {
+//     aliases: ["awk", "awklang", "awk-lang"],
+//     keywords: {
+//         keyword: "if else print break printf while for in return skip BEGIN END",
+//         literal: "",
+//         built_in: ""
+//     },
+//     illegal: "</",
+//   contains: [ hljs.COMMENT('#', '$'), ]
+// }
+// });
+//
+// hljs.registerLanguage("dtrace", function(e) {
+// return {
+//     aliases: ["d", "dtracelang", "dtrace-lang"],
+//     keywords: {
+//         keyword: "if else self this BEGIN END typedef struct uintptr_t uint8_t uint16_t uint32_t uint64_t intptr_t int8_t int16_t int32_t int64_t size_t void",
+//         literal: "",
+//         built_in: "trace copyin copyinstr stringof uregs print printf exit rindex strlen quantize lquantize timestamp execname pid $target io start args"
+//     },
+//     illegal: "</",
+//   contains: [ hljs.COMMENT('//', '$'), ]
+// }
+// });
+//
+// hljs.highlightAll();
 
 document.body.style['color-scheme'] = 'light dark';
 let colorScheme = localStorage.getItem('colorScheme');
@@ -94,51 +94,7 @@ dark_light_mode_button.addEventListener('click', function(e) {
 });
 
 document.querySelectorAll('code').forEach((el, _i) => {
-  if (el.parentElement.tagName == "PRE"){
-    var header = document.createElement('div');
-    header.classList.add('code-header');
-
-    var header_text = document.createElement('span');
-    var lang = 'text';
-    var prefix = 'language-';
-    for (c of el.classList) {
-      if (c.startsWith(prefix)) {
-        lang = c.slice(prefix.length);
-        break;
-      }
-    }
-    header_text.innerText = lang[0].toUpperCase() + lang.slice(1);
-    header_text.style['margin-left'] = '.2rem';
-    header.appendChild(header_text);
-
-
-    var copy_btn = document.createElement('button');
-    copy_btn.innerHTML = `<svg aria-hidden="true" focusable="false" class="octicon octicon-copy" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align: text-bottom;"><path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path></svg>`;
-    copy_btn.classList.add('copy-code');
-    copy_btn.type = 'button';
-
-    // Copy original content before adding line numbers.
-    var content = el.innerText.slice();
     copy_btn.addEventListener('click', function(e){
       navigator.clipboard.writeText(content);
     });
-    header.appendChild(copy_btn);
-    el.parentElement.prepend(header);
-  }
-
-
-  if (0 == el.classList.length || el.classList.contains('language-sh') || el.classList.contains('language-shell') || el.classList.contains('language-bash') || el.classList.contains('language-ini') || el.classList.contains('language-diff')){ 
-    return; 
-  }
-
-
-  var lines = el.innerHTML.trimEnd().split('\n');
-  if (lines.length <= 1) {
-    return;
-  }
-  var out = [];
-  lines.forEach(function(l, i){
-    out.push('<span class="line-number">' + (i+1).toString() + '</span> ' + l);
-  });
-  el.innerHTML = out.join('\n');
 });
--- src/main.rs
+++ src/main.rs
@@ -533,6 +533,16 @@ fn md_to_html(md_content: &str, html_content: &mut Vec<u8>) {
     md_render_footnote_definitions(html_content, &footnote_defs);
 }
 
+fn capitalize_first(s: &str) -> Cow<'_, str> {
+    let mut chars = s.chars();
+    let c = chars.next().unwrap();
+    if c.is_uppercase() {
+        Cow::Borrowed(s)
+    } else {
+        Cow::Owned(c.to_uppercase().collect::<String>() + chars.as_str())
+    }
+}
+
 fn md_to_html_rec(
     content: &mut Vec<u8>,
     footnote_defs: &mut Vec<FootnoteDefinition>,
@@ -645,14 +655,24 @@ fn md_to_html_rec(
         }
         Node::Code(code) => {
             let sanitized = text_sanitize_for_html(&code.value, false);
-            writeln!(
+            let lang = code.lang.as_ref().unwrap();
+            write!(
                 content,
-                r#"<pre><code class="language-{}">{}
-</code></pre>"#,
-                code.lang.as_ref().unwrap(),
-                sanitized
+                r#"<pre>
+  <div class="code-header">
+    <span>{}</span>
+    <button class="copy-code" type="button"><svg aria-hidden="true" focusable="false" class="octicon octicon-copy" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align: text-bottom;"><path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path></svg></button>
+  </div>
+  <code class="language-{}">"#,
+                capitalize_first(lang),
+                lang,
             )
             .unwrap();
+
+            for (i, line) in sanitized.lines().enumerate() {
+                writeln!(content, r#"<span class="line-number">{}</span>{}"#, i, line).unwrap();
+            }
+            writeln!(content, "</code></pre>").unwrap();
         }
         Node::Math(_math) => todo!(),
         Node::MdxFlowExpression(_mdx_flow_expression) => todo!(),
